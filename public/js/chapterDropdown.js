676c148fa7a3207b1339fc0f32f4b2ada6598329
christian <nicochridevs@gmail.com>
2018-08-10 00:17:12 +0200

    Hover multiple choice buttons working with javascript

--- public/js/chapterDropdown.js
+++ public/js/chapterDropdown.js
@@ -1,12 +1,44 @@
-function showDiv(){
-	var myStringArray = document.getElementsByClassName('chapterDropdown');
-	var arrayLength = myStringArray.length;
-	for (var i = 0; i < arrayLength; i++) {
-		if (myStringArray[i].style.display === "none"){
-			myStringArray[i].style.display = "block";
-		}
-		else{
-			myStringArray[i].style.display = "none";
-		}
+function innerCircle(optionNumber) {
+	if (optionNumber == 1) {
+		var option1 = document.getElementById('option1');
+		option1.style.backgroundImage = "url('/static/hoverCircle.png')";
+	}
+	
+	else if (optionNumber == 2) {
+		var option2 = document.getElementById('option2');
+		option2.style.backgroundImage = "url('/static/hoverCircle.png')";
+	}
+	
+	else if (optionNumber == 3) {
+		var option3 = document.getElementById('option3');
+		option3.style.backgroundImage = "url('/static/hoverCircle.png')";
+	}
+	
+	else {
+		var option4 = document.getElementById('option4');
+		option4.style.backgroundImage = "url('/static/hoverCircle.png')";	
+	}
+	
+}
+
+function innerCircleOff(optionNumberOff) {
+	if (optionNumberOff == 1) {
+		var option1 = document.getElementById('option1');	
+		option1.style.backgroundImage = "url('/static/ring.png')";
+	}
+	
+	else if (optionNumberOff == 2) {
+		var option2 = document.getElementById('option2');
+		option2.style.backgroundImage = "url('/static/ring.png')";
+	}
+	
+	else if (optionNumberOff == 3) {
+		var option3 = document.getElementById('option3');
+		option3.style.backgroundImage = "url('/static/ring.png')";
+	}
+
+	else {
+		var option4 = document.getElementById('option4');
+		option4.style.backgroundImage = "url('/static/ring.png')";
 	}
 }
