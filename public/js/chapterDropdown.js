7b516e880e20542fa7a5b470238f1ae874287e80
christian <nicochridevs@gmail.com>
2018-08-10 15:29:54 +0200

    Questions box and some multiple choice boxes javascript

--- public/js/chapterDropdown.js
+++ public/js/chapterDropdown.js
@@ -1,44 +1,68 @@
-function innerCircle(optionNumber) {
-	if (optionNumber == 1) {
-		var option1 = document.getElementById('option1');
-		option1.style.backgroundImage = "url('/static/hoverCircle.png')";
-	}
-	
-	else if (optionNumber == 2) {
-		var option2 = document.getElementById('option2');
-		option2.style.backgroundImage = "url('/static/hoverCircle.png')";
-	}
-	
-	else if (optionNumber == 3) {
-		var option3 = document.getElementById('option3');
-		option3.style.backgroundImage = "url('/static/hoverCircle.png')";
-	}
-	
-	else {
-		var option4 = document.getElementById('option4');
-		option4.style.backgroundImage = "url('/static/hoverCircle.png')";	
-	}
-	
+function changeBackground1() {
+	var el1 = document.getElementById('option1');
+	el1.style.backgroundImage = "url('/static/clickedCircle.png')";
+	var el2 = document.getElementById('option2');
+	var el2div = document.getElementById('option2div');
+	el2.style.backgroundImage = "url('/static/ring.png')";
+	el2div.classList.remove("active");
+	var el3 = document.getElementById('option3');
+	var el3div = document.getElementById('option3div');
+	el3div.classList.remove("active");
+	el3.style.backgroundImage = "url('/static/ring.png')";
+	var el4 = document.getElementById('option4');
+	var el4div = document.getElementById('option4div');
+	el4.style.backgroundImage = "url('/static/ring.png')";
+	el4div.classList.remove("active");
 }
 
-function innerCircleOff(optionNumberOff) {
-	if (optionNumberOff == 1) {
-		var option1 = document.getElementById('option1');	
-		option1.style.backgroundImage = "url('/static/ring.png')";
-	}
-	
-	else if (optionNumberOff == 2) {
-		var option2 = document.getElementById('option2');
-		option2.style.backgroundImage = "url('/static/ring.png')";
-	}
-	
-	else if (optionNumberOff == 3) {
-		var option3 = document.getElementById('option3');
-		option3.style.backgroundImage = "url('/static/ring.png')";
-	}
+function changeBackground2() {
+	var el1 = document.getElementById('option1');
+	var el1div = document.getElementById('option1div');
+	el1.style.backgroundImage = "url('/static/ring.png')";
+	el1div.classList.remove("active");
+	var el2 = document.getElementById('option2');
+	el2.style.backgroundImage = "url('/static/clickedCircle.png')";
+	var el3 = document.getElementById('option3');
+	el3.style.backgroundImage = "url('/static/ring.png')";
+	var el3div = document.getElementById('option3div');
+	el3div.classList.remove("active");
+	el3.style.backgroundImage = "url('/static/ring.png')";
+	var el4 = document.getElementById('option4');
+	var el4div = document.getElementById('option4div');
+	el4.style.backgroundImage = "url('/static/ring.png')";
+	el4div.classList.remove("active");
+}
 
-	else {
-		var option4 = document.getElementById('option4');
-		option4.style.backgroundImage = "url('/static/ring.png')";
-	}
+function changeBackground3() {
+	var el1 = document.getElementById('option1');
+	var el1div = document.getElementById('option1div');
+	el1.style.backgroundImage = "url('/static/ring.png')";
+	el1div.classList.remove("active");
+	var el2 = document.getElementById('option2');
+	var el2div = document.getElementById('option2div');
+	el2.style.backgroundImage = "url('/static/ring.png')";
+	el2div.classList.remove("active");
+	var el3 = document.getElementById('option3');
+	el3.style.backgroundImage = "url('/static/clickedCircle.png')";
+	var el4 = document.getElementById('option4');
+	var el4div = document.getElementById('option4div');
+	el4.style.backgroundImage = "url('/static/ring.png')";
+	el4div.classList.remove("active");
+}
+
+function changeBackground4() {
+	var el1 = document.getElementById('option1');
+	var el1div = document.getElementById('option1div');
+	el1.style.backgroundImage = "url('/static/ring.png')";
+	el1div.classList.remove("active");
+	var el2 = document.getElementById('option2');
+	var el2div = document.getElementById('option2div');
+	el2.style.backgroundImage = "url('/static/ring.png')";
+	el2div.classList.remove("active");
+	var el3 = document.getElementById('option3');
+	var el3div = document.getElementById('option3div');
+	el3div.classList.remove("active");
+	var el4 = document.getElementById('option4');
+	el3.style.backgroundImage = "url('/static/ring.png')";
+	el4.style.backgroundImage = "url('/static/clickedCircle.png')";
 }
