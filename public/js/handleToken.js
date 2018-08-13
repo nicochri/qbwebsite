135b5a3a74c3942acabdd305252877c16ae97848
nicochri <nicochridevs@gmail.com>
2018-08-13 11:55:13 +0200

    cleanup

--- public/js/handleToken.js
+++
@@ -1,3 +0,0 @@
-// var t = document.getElementsByClassName('token')[0];
-// console.log(t.innerText);
-
--- public/js/payment.js
+++
@@ -1,6 +0,0 @@
-const payment = document.getElementById('payment');
-if(payment) {
-	get('/api/whoami', {}, function(user) {
-    	const gid = document.getElementById('gid').value = user.gid;
-  	});
-}
