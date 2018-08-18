9900e5cb2124f7fd9672b73e9d8b5dc952bf5a98
nicochri <nicochridevs@gmail.com>
2018-08-18 09:32:05 +0200

    load questions automatically

--- public/js/insertQuestions.js
+++ public/js/insertQuestions.js
@@ -1,31 +1,57 @@
-$(document).ready(function(){
+function addQuestion(questionId, questionNumber, question) {
 	//Create question sidebar element
-	var question = document.createElement("a");
-	question.setAttribute("class", "list-group-item list-group-item-action");
-	question.setAttribute("id", "list-halla-list");
-	question.setAttribute("data-toggle", "list");
-	question.setAttribute("href", "#list-halla");
-	question.setAttribute("role", "tab");
-	question.setAttribute("aria-controls", "settings");
-	question.innerHTML = "Halla";
+	var questionSidebar = document.createElement("a");
+	questionSidebar.setAttribute("class", "list-group-item list-group-item-action");
+	questionSidebar.setAttribute("id", "list-" + questionId + "-list");
+	questionSidebar.setAttribute("data-toggle", "list");
+	questionSidebar.setAttribute("href", "#list-" + questionId);
+	questionSidebar.setAttribute("role", "tab");
+	questionSidebar.setAttribute("aria-controls", "settings");
+	questionSidebar.innerHTML = questionNumber + ". " + question.title;
     
 	//Create question div
 	var questionDiv = document.createElement("a");
-	questionDiv.setAttribute("class", "tab-pane fade");
-	questionDiv.setAttribute("id", "list-halla");
+	questionDiv.setAttribute("class", "tab-pane");
+	questionDiv.setAttribute("id", "list-" + questionId);
 	questionDiv.setAttribute("role", "tabpanel");
-	questionDiv.setAttribute("aria-controls", "list-halla-list");
+	questionDiv.setAttribute("aria-controls", "list-" + questionId + "-list");
 
-	var header = document.createElement("p");
-	header.setAttribute("id", "header");
-	header.innerHTML = "If $ax^2+bx+c=0$, then $x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}$. When $a \\ne 0$, there are two solutions to \\(ax^2 + bx + c = 0\\) and they are $$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$";
+	var questionPar = document.createElement("p");
+	questionPar.setAttribute("id", "header");
+	questionPar.innerHTML = question.text;
+
+	if (questionNumber == 1) {
+		questionSidebar.classList.add("active");
+		questionDiv.classList.add("active");
+		questionDiv.classList.add("show");
+
+		$('#optionA').html(question.options.A);
+		$('#optionB').html(question.options.B);
+		$('#optionC').html(question.options.C);
+		$('#optionD').html(question.options.D);
+	}
 
 	//Append elements
-    $('#list-tab').append(question);
+    $('#list-tab').append(questionSidebar);
     $('#nav-tabContent-questions').append(questionDiv);
-	$('#list-halla').append(header);
-    //
-    // var math = document.getElementById("header");
-	// $('#header').html("If $ax^2+bx+c=0$, then $x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}$.");
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub,header]);
+	$('#list-' + questionId).append(questionPar);
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,questionPar]);
+
+    //Change option values if question is clicked
+    $("#" + "list-" + questionId + "-list").click(function(){
+	    $('#optionA').html(question.options.A);
+		$('#optionB').html(question.options.B);
+		$('#optionC').html(question.options.C);
+		$('#optionD').html(question.options.D);
+	});
+}
+
+$(document).ready(function(){
+	var questionNumber = 1;
+	$.getJSON( "/static/js/jsondata.js", function( data ) {
+    	$.each(data, function(key, val) {
+    		addQuestion(key, questionNumber, val);
+    		questionNumber = questionNumber + 1;
+    	});
+    });
 });
---
+++ public/js/jsondata.js
@@ -0,0 +1,13 @@
+{
+  "1": {
+  	"title": "Quadratic equation", 
+  	"text": "When $a \\ne 0$, there are two solutions to \\(ax^2 + bx + c = 0\\) and they are$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$",
+  	"options": {"A": "10", "B": "12", "C": "30", "D": "44"}
+  },
+
+  "2": {
+  	"title": "Wassup", 
+  	"text": "hey dude",
+  	"options": {"A": "1", "B": "2", "C": "3", "D": "4"}
+  }
+}
