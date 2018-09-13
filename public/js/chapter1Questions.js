65acb144a54e98e7b84831fef412dc87aa18cf1b
nicochri <nicochridevs@gmail.com>
2018-09-13 09:06:29 +0200

    starting working on questions

---
+++ public/js/chapter1Questions.js
@@ -0,0 +1,43 @@
+{
+    "id": "1",
+    "chapter": "1",
+  	"title": "Quadratic equation", 
+  	"text": "This is the first wuestions When $a \\ne 0$, there are two solutions to \\(ax^2 + bx + c = 0\\) and they are$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$",
+    "solution": "This is the first loaded solution from the json file",
+  	"options": {"A": "10", "B": "12", "C": "30", "D": "44"},
+    "correctOption": "A",
+    "difficulty": "success"
+},
+
+{
+    "id": "",
+    "chapter": "1",
+  	"title": "", 
+  	"text": "",
+    "solution": "",
+  	"options": {"A": "", "B": "", "C": "", "D": ""},
+    "correctOption": "",
+    "difficulty": "success"
+},
+
+{
+    "id": "",
+    "chapter": "1",
+  	"title": "", 
+  	"text": "",
+    "solution": "",
+  	"options": {"A": "", "B": "", "C": "", "D": ""},
+    "correctOption": "",
+    "difficulty": "success"
+},
+
+{
+    "id": "",
+    "chapter": "1",
+  	"title": "", 
+  	"text": "",
+    "solution": "",
+  	"options": {"A": "", "B": "", "C": "", "D": ""},
+    "correctOption": "",
+    "difficulty": "success"
+}
