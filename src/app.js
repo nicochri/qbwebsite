ef4b1cb6be6e02f351536d8a6c47bb2a362d7ed1
nicochri <nicochridevs@gmail.com>
2018-09-16 23:40:18 +0200

    error load pages

--- src/app.js
+++ src/app.js
@@ -81,18 +81,18 @@ app.get(
 app.use(function(req, res, next) {
   const err = new Error(' Not Found');
   err.status = 404;
-  // next(err);
-  res.redirect('/');
+  next(err);
+  // res.redirect('/');
 });
 
 // route error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  // res.send({
-  //   status: err.status,
-  //   message: err.message,
-  // });
-  res.redirect('/');
+  res.send({
+    status: err.status,
+    message: err.message,
+  });
+  // res.redirect('/');
 });
 
 // port config
